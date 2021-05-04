e7cf1d0ddd35857b5d35ce58777b14bba6ab2d12
DESKTOP-THVMA8D\Admin <khd>
2021-05-04 17:10:37 +0900

    no message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const { User } = require('./models/User');
 
@@ -12,6 +13,7 @@ const { User } = require('./models/User');
 app.use(bodyParser.urlencoded({extended: true}));
 //application/x-www-form-urlencoded
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose.connect( config.mongoURI , {
@@ -36,6 +38,32 @@ app.post('/register',(req,res) => {
         })
     });
 });
+app.post('/login',(req,res)=>{
+    //요청된 이메일을 데이터베이스에서 찾기
+    User.findOne({ email: req.body.email },(err,user)=>{
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                massage: "제공된 이메일에 해당하는 유저가 없습니다",
+            })
+        }
+     //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인
 
+        user.comparePassword(req.body.comparePassword, (err,isMatch)=>{
+            if(!isMatch){
+                return res.json({loginSuccess: false, message: '비밀번호땡'})
+            }
+            //비밀번호 까지 맞다면 토큰을 생성하기.
+            user.generateToken((err,user)=>{
+                if(err) return res.status(400).send(err);
+                //토큰을 저장한다. 어디에
+                res.cookie('x_auth', user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id});
+            })
+        })
+    });
+    
+});
 
 app.listen(port, () => console.log(`hello ${port}`));
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwr = require('jsonwebtoken');
 
 
 const userSchema = new mongoose.Schema({
@@ -32,6 +35,47 @@ const userSchema = new mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save',function(next){
+    //비밀번호를 암호 시킵니다
+    var user = this;
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if(err) return next(err)
+    
+            bcrypt.hash(user.password , salt , function(err,hash) {
+                if(err) return next(err)
+                user.password = hash;
+                next();
+            })
+        })
+    } else{
+        next();
+    }
+    
+})
+
+userSchema.method.comparePassword = function(plainPassword, cd){
+    //plainPassword
+    bcrypt.compare(plainPassword, this.password, function(err,isMatch){
+        if(err) return cd(err),
+        cd(null,isMatch)
+    })
+}
+
+userSchema.method.generateToken = function(cd){
+    var user = this;
+    //jontoken 생성
+    var token = jwt.sign(user._id.toHexString(),'secretToken');
+
+    user.token = token;
+    user.save(function(err,user){
+        if(err) return cd(err)
+        cd(null , user)
+    })
+
+
+}
 
 const User = mongoose.model('User',userSchema)
 
